20c4784c39c1fa17dc77d498e2b237cb7c11f221
blueshadow3106-coder <blue.shadow3106@gmail.com>
2025-11-05 18:20:52 +0100

    Entrainement_interro.py
    Les derniers exercices qui n'apparaissaient pas dans la correction(le code a fonctionné pour moi)

--- NSI_1ere/Python/Entrainement_interro.py
+++ NSI_1ere/Python/Entrainement_interro.py
@@ -34,4 +34,44 @@ def positions_lettre(chaine, lettre):
     for i in range(len(chaine)):
         if chaine[i] == lettre:
             res.append(i)
+
     return res
+    
+#Exo 6
+def masquer(mot):
+    nv=""
+    for i in mot :
+        if i in "aeiouy":
+            nv=nv+"*"
+        else:
+            nv=nv+i
+    return(nv)
+            
+#Exo 7
+import math
+nombres=[5,6,4,2,4]
+min=math.inf
+max=0
+for i in nombres:
+    if i>max:
+        max=i
+    elif i<min:
+        min=i
+print (max , min)
+
+#Exo 8
+nombres=[1,5,8,6,2]
+p=[]
+im=[]
+for i in nombres:
+    if (i%2)==0:
+        p.append(i)
+    else:
+        im.append(i)
+print(im,p)
+
+#Exo 9 :
+def affiche(chaine):
+    for i in range(len(chaine)):
+        print(i,chaine[i])
+    return
